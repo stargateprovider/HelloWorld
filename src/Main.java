4a479b52ac3f28b9c0ccb9d6eb02424b6d9ffdb4
unknown <lemmo.lavonen@gmail.com>
2020-03-01 18:05:03 +0200

    typo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I can neither confirm or deny ever being here.");
+        System.out.println("I can neither confirm nor deny ever being here.");
     }
 }
