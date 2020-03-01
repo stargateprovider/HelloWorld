8127121873592f96290c9bd069babb721688f94c
unknown <lemmo.lavonen@gmail.com>
2020-03-01 18:03:21 +0200

    doing friend's duty

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("goodbye world");
+        System.out.println("I can neither confirm or deny ever being here.");
     }
 }
